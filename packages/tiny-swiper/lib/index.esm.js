d7fbf5f2c3a62060e65963b78a73927b70ea3a2d
hezaichang <heyin223@gmail.com>
2020-12-18 21:53:20 +0800

    Fix: #46 Mousewheel Swipe Too Fast

--- packages/tiny-swiper/lib/index.esm.js
+++ packages/tiny-swiper/lib/index.esm.js
@@ -307,26 +307,20 @@ function SwiperPluginMousewheel(instance, options) {
     wheelingTimer: 0
   };
 
-  var initWheelStatus = function initWheelStatus() {
-    wheelStatus.wheeling = false;
-    wheelStatus.wheelDelta = 0;
-    wheelStatus.wheelingTimer = 0;
-  };
-
   var handler = function handler(e) {
-    var delta = options.isHorizontal ? e.deltaX : e.deltaY;
+    var delta = Math.abs(e.deltaX) > Math.abs(e.deltaY) ? e.deltaX : e.deltaY;
     var index = instance.state.index;
 
-    if ((Math.abs(delta) - Math.abs(wheelStatus.wheelDelta) > 0 || !wheelStatus.wheeling) && Math.abs(delta) >= options.mousewheel.sensitivity) {
-      instance.slideTo(delta > 0 ? index - 1 : index + 1);
+    if (Math.abs(delta) - Math.abs(wheelStatus.wheelDelta) > 0 && !wheelStatus.wheeling && Math.abs(delta) >= options.mousewheel.sensitivity) {
+      var step = options.mousewheel.invert ? -1 : 1;
+      instance.slideTo(delta > 0 ? index - step : index + step);
+      wheelStatus.wheeling = true;
+      wheelStatus.wheelingTimer = setTimeout(function () {
+        wheelStatus.wheeling = false;
+      }, options.mousewheel.interval);
     }
 
     wheelStatus.wheelDelta = delta;
-    clearTimeout(wheelStatus.wheelingTimer);
-    wheelStatus.wheeling = true;
-    wheelStatus.wheelingTimer = setTimeout(function () {
-      initWheelStatus();
-    }, options.mousewheel.interval);
     e.preventDefault();
     e.stopPropagation();
   };
--- packages/tiny-swiper/src/modules/mousewheel.ts
+++ packages/tiny-swiper/src/modules/mousewheel.ts
@@ -31,27 +31,30 @@ export default function SwiperPluginMousewheel (
         wheelDelta: 0,
         wheelingTimer: 0
     }
-    const initWheelStatus = () => {
-        wheelStatus.wheeling = false
-        wheelStatus.wheelDelta = 0
-        wheelStatus.wheelingTimer = 0
-    }
     const handler = (e: WheelEvent) => {
-        const delta = options.isHorizontal ? e.deltaX : e.deltaY
+        const delta = Math.abs(e.deltaX) > Math.abs(e.deltaY)
+            ? e.deltaX
+            : e.deltaY
         const {
             index
         } = instance.state
 
-        if ((Math.abs(delta) - Math.abs(wheelStatus.wheelDelta) > 0 || !wheelStatus.wheeling)
-            && Math.abs(delta) >= options.mousewheel.sensitivity) {
-            instance.slideTo(delta > 0 ? index - 1 : index + 1)
+        if ((Math.abs(delta) - Math.abs(wheelStatus.wheelDelta) > 0)
+            && !wheelStatus.wheeling
+            && Math.abs(delta) >= options.mousewheel.sensitivity
+        ) {
+            const step = options.mousewheel.invert ? -1 : 1
+
+            instance.slideTo(delta > 0
+                ? index - step
+                : index + step
+            )
+            wheelStatus.wheeling = true
+            wheelStatus.wheelingTimer = setTimeout(() => {
+                wheelStatus.wheeling = false
+            }, options.mousewheel.interval) as unknown as number
         }
         wheelStatus.wheelDelta = delta
-        clearTimeout(wheelStatus.wheelingTimer)
-        wheelStatus.wheeling = true
-        wheelStatus.wheelingTimer = setTimeout(() => {
-            initWheelStatus()
-        }, options.mousewheel.interval) as unknown as number
         e.preventDefault()
         e.stopPropagation()
     }
